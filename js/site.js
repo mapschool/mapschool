4c0b1da960c119ec44dac9485387b111ff44fc92
Hannes <kannes@users.noreply.github.com>
2018-06-20 21:56:43 +0200

    turn header into anchor, code comments
    via http://blog.parkermoore.de/2014/08/01/header-anchor-links-in-vanilla-javascript-for-github-pages-and-jekyll/ might be nicer

--- js/site.js
+++ js/site.js
@@ -1,8 +1,12 @@
 var headers = document.querySelectorAll('h2, h3, h4');
 
 for (var i = 0; i < headers.length; i++) {
+    // set header id from its content
     headers[i].id = headers[i].innerHTML
         .replace(/^\s+|\s+$/g, '')
         .toLowerCase()
         .replace(/[^\w]/g, '-');
+
+    // turn header into anchor
+    headers[i].innerHTML = headers[i].innerHTML + '<a href="#' + headers[i].id + '" class="header-link"></a>';
 }
